6d5f7ac96675f08fcf1f58d0b3d7251947562fbb
ateliersjp <za99jp@gmail.com>
2025-09-03 13:38:27 +0900

    Initial commit

---
+++ background.js
@@ -0,0 +1,44 @@
+const instance = {};
+
+chrome.webNavigation.onBeforeNavigate.addListener(
+  (details) => {
+    const url = new URL(details.url);
+    if (url.hostname !== instance.value) {
+      if (url.hostname === "x.com" || url.hostname === "twitter.com") {
+        url.hostname = instance.value || "twitt.jp";
+        chrome.tabs.update(details.tabId, { url: url.href });
+      }
+    }
+  },
+  { urls: ["https://*/*"] },
+);
+
+chrome.webNavigation‎.onHeadersReceived.addListener(
+  (details) => {
+    const url = new URL(details.url);
+    if (url.hostname === "twitt.jp") {
+      if (details.statusCode === 301 || details.statusCode === 302) {
+        const locationHeader = details.responseHeaders.find(header => header.name.toLowerCase() === 'location');
+        if (locationHeader) {
+          const locationUrl = new URL(locationHeader.value);
+          instance.value = locationUrl.hostname;
+        }
+      }
+    }
+  },
+  { urls: ["https://*/*"] },
+  ["responseHeaders"]
+);
+
+chrome.webNavigation‎.onCompleted.addListener(
+  (details) => {
+    const url = new URL(details.url);
+    if (url.hostname === instance.value) {
+      if (!details.statusCode || details.statusCode === 429) {
+        url.hostname = "twitt.jp";
+        chrome.tabs.update(details.tabId, { url: url.href });
+      }
+    }
+  },
+  { urls: ["https://*/*"] }
+);
